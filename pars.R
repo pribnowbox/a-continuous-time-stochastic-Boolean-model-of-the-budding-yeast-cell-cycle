8fe1ebe85439809fdf76af286527fb4019c4c179
pribnowbox <laomett@vt.edu>
2022-06-23 09:04:15 +0700

    Add files via upload

---
+++ pars.R
@@ -0,0 +1,16 @@
+# Parameters
+pG1=1;
+pCdh1=1; 
+pSBF=1; 
+pCln2=10; 
+pClb5=1; 
+pClb2G=1; 
+pClb2M=1; 
+pCdc20=1
+mu=0.007;
+tM.mean=30
+tM.CV=0.30
+S0.mean=0.4;
+S0.CV=0.05;
+f.mean=0.58;
+f.CV=0.05;
